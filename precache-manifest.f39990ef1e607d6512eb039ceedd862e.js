64a7d6ad80e3183685afdc6aa77b1a11ab2f8caf
Leel Usama <leelusama@gmail.com>
2020-06-01 13:51:49 +0500

    Updates

---
+++ precache-manifest.f39990ef1e607d6512eb039ceedd862e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c095297e0363b9330c81b50b95c482",
+    "url": "/eve-sp-farm/index.html"
+  },
+  {
+    "revision": "344bbfd409fcf291bc70",
+    "url": "/eve-sp-farm/static/css/main.b668d98f.chunk.css"
+  },
+  {
+    "revision": "14111b79cc444669f134",
+    "url": "/eve-sp-farm/static/js/2.0d4ffaa7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eve-sp-farm/static/js/2.0d4ffaa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "344bbfd409fcf291bc70",
+    "url": "/eve-sp-farm/static/js/main.9b795fa1.chunk.js"
+  },
+  {
+    "revision": "4a41ba9863a51be42ac6",
+    "url": "/eve-sp-farm/static/js/runtime-main.d6272f2b.js"
+  }
+]);
